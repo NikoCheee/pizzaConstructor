3defd5157bffeecbd400a44b302d2f028e6aa8b2
Ni_Che <nikita3ch@gmail.com>
2023-03-21 14:41:26 +0200

    registered Sauce and CheeseBoard in admin.py

--- pizzaConstructorApp/admin.py
+++ pizzaConstructorApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import PizzaOrder, Ingredient, PizzaToppings, Size, Category
+from .models import PizzaOrder, Ingredient, PizzaToppings, Size, Category, Sauce, CheeseBoard
 
 admin.site.register(Ingredient)
 admin.site.register(PizzaOrder)
 admin.site.register(PizzaToppings)
 admin.site.register(Size)
 admin.site.register(Category)
+admin.site.register(Sauce)
+admin.site.register(CheeseBoard)
